0ab2f3bdb7e7c9b248eee50e2b1be0373a91798f
Andrii Susenko <susenko.andrii@gmail.com>
2025-01-23 22:43:59 +0200

    Add migrations

---
+++ olx-parser-app/database/migrations/2025_01_23_203635_create_links_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('links', function (Blueprint $table) {
+            $table->id(); // Primary key
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade'); 
+            $table->string('url_link'); 
+            $table->decimal('last_price', 10, 2)->nullable(); 
+            $table->timestamps(); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('links');
+    }
+}
---
+++ olx-parser-app/database/migrations/2025_01_23_203643_create_email_notifications_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmailNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('email_notifications', function (Blueprint $table) {
+            $table->id(); // Primary key
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade'); // Foreign key to users table
+            $table->timestamp('notification_sent_at'); // Timestamp of the notification
+            $table->decimal('price', 10, 2); // Price at the time of notification
+            $table->timestamps(); // Created at and updated at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('email_notifications');
+    }
+}
