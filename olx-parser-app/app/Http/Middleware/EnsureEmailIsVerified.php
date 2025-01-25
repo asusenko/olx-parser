c89960a21b13b5f75c2ae123f4368879ed2611d4
Andrii Susenko <susenko.andrii@gmail.com>
2025-01-25 15:18:39 +0200

    Email should be verifided middleware add

---
+++ olx-parser-app/app/Http/Middleware/EnsureEmailIsVerified.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureEmailIsVerified
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!$request->user() || !$request->user()->hasVerifiedEmail()) {
+            // Redirect to email verification notice if email is not verified
+            return redirect()->route('verification.notice');
+        }
+
+        return $next($request);
+    }
+}
