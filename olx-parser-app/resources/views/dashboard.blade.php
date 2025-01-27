28f3922c4ba9f2c1841ee352783b6745284bd5cc
Andrii Susenko <susenko.andrii@gmail.com>
2025-01-27 15:58:45 +0200

    Add del and get routs

--- olx-parser-app/resources/views/dashboard.blade.php
+++ olx-parser-app/resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Дашборд') }}
         </h2>
     </x-slot>
 
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <h3 class="text-lg font-medium">Subscribed Links</h3>
+                    <h3 class="text-lg font-medium">Підписка на посилання</h3>
                     <ul>
                         @forelse ($links as $link)
                             <li>
@@ -19,7 +19,7 @@
                                 <span class="text-sm text-gray-500">Last Price: {{ $link->last_price }}</span>
                             </li>
                         @empty
-                            <li class="text-gray-500">You have no subscriptions yet.</li>
+                            <li class="text-gray-500">Ще поки немає посилань</li>
                         @endforelse
                     </ul>
                 </div>
